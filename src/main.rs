2bb61c12e28325c768985f067bf8d3c2e624160e
Your Name <you@example.com>
2022-04-26 00:18:19 +0800

    add check_install function

--- src/main.rs
+++ src/main.rs
@@ -13,37 +13,46 @@ fn main() {
         return;
     }
 
+    // 拼接args成一个字符串，忽略第一个元素
+    let cmd = args.iter().skip(1).fold(String::new(), |mut acc, x| {
+        acc.push_str(&x);
+        acc.push_str(" ");
+        acc
+    });
+    let cmd = cmd.trim();
+    dump!(cmd);
+
     // 如果参数二包含youtube链接，则解析链接，并下载视频
-    if args[1].contains("youtube.com") {
-        let video_url = args[1].to_string();
-        let command = format!("yt-dlp {}", video_url);
-        run(command);
+    if cmd.contains("youtube.com") {
+        run(format!("yt-dlp {}", cmd));
     }
 
     // 如果参数二包含drive.google.com链接，使用gdown下载
-    if args[1].contains("drive.google.com") {
-        let url = args[1].to_string();
-
-        // 判断gdown是否安装，如果没有安装，则询问是否安装gdown
-        if run_quite(format!("which gdown")).success() == false {
-            println!("gdown not installed, install it now? (y/n)");
-            let mut input = String::new();
-            std::io::stdin().read_line(&mut input).expect("Failed to read line");
-            // 如果input包含y，则安装gdown
-            if input.contains("y") {
-                println!("Installing gdown...");
-                run(format!("pip install gdown"));
-                let _ = Command::new("pip").arg("install").arg("gdown");
-            } else {
-                println!("Please install gdown first.");
-                return;
-            }
-        }
-
-        run(format!("gdown {}", url));
+    if cmd.contains("drive.google.com") {
+        check_install("gdown","pip install gdown");
+        run(format!("gdown {}", cmd));
     }
     
 }
+
+// 检测安装软件是否符合要求
+fn check_install(software: &str,install_command: &str) {
+    // 判断gdown是否安装，如果没有安装，则询问是否安装gdown
+    if run_quite(format!("which {}",software)).success() == false {
+        println!("{} not installed, install it now? (y/n)", software);
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).expect("Failed to read line");
+        // 如果input包含y，则安装gdown
+        if input.contains("y") {
+            run(format!("{}", install_command));
+        } else {
+            println!("Please install {} first.", software);
+            // 程序退出
+            std::process::exit(1);
+        }
+    }
+}
+    
 
 fn run(command : String) -> ExitStatus {
     Command::new("sh").arg("-c").arg(command).status().unwrap()
